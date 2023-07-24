c3953e760c85aff0a3c64fa68efb48cf4534d79e
oxone_999 <anujverma11062002@gmail.com>
2023-07-24 11:16:40 +0530

    asdds

--- frontend/src/Components/IdleHours/IdleHours.jsx
+++ frontend/src/Components/IdleHours/IdleHours.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
-import Styles from "../../Styles/tasks.module.css";
+import Styles from "../../Styles/idleHours.module.css";
 import { formatDistanceToNow } from "date-fns";
 
 const IdleHours = () => {
--- frontend/src/Components/Report/Report.jsx
+++ frontend/src/Components/Report/Report.jsx
@@ -105,116 +105,126 @@ function Report() {
       <ToastContainer />
       <div className={Styles.container}>
         <div className={Styles.heading}>IDLE SITUATION</div>
-        <div className={Styles.section}>
-          <div className={Styles.content}>
-            <p>Date</p>
-            <DatePicker
-              selected={selectedDate}
-              onChange={handleDateChange}
-              dateFormat="dd/MM/yyyy"
-              placeholderText="Select a date"
-              className={Styles.dateInput}
-            />
-            <button onClick={handleFillDate}>Current</button>
-          </div>
-          <div className={Styles.content}>
-            <p>Shift No. : </p>
-            <select
-              value={shift}
-              onChange={handleShift}
-              className={Styles.input}
-            >
-              <option value="">Select an option</option>
-              <option value="option1">1</option>
-              <option value="option2">2</option>
-              <option value="option3">3</option>
-            </select>
-          </div>
-          <div className={Styles.content}>
-            <p>Vehicle : </p>
-            <select
-              value={vehicle}
-              onChange={handleVehicle}
-              className={Styles.input}
-              style={{ width: "7rem" }}
-            >
-              <option value="">Excavator</option>
-              <option value="Ve-3">Ve-3</option>
-              <option value="Ve-4">Ve-4</option>
-              <option value="Ve-5">Ve-5</option>
-              <option value="Ve-6">Ve-6</option>
-              <option value="Ve-7">Ve-7</option>
-              <option value="KVE-9">KVE-9</option>
-              <option value="KVE-10">KVE-10</option>
-              <option value="KVE-11">KVE-11</option>
-              <option value="KVE-12">KVE-12</option>
-              <option value="KVE-13">KVE-13</option>
-              <option value="KVE-14">KVE-14</option>
-            </select>
+        <form style={{ margin: "0", padding: "0" }} onSubmit={handleSubmit}>
+          <div className={Styles.section}>
+            <div className={Styles.content}>
+              <p>Date</p>
+              <DatePicker
+                selected={selectedDate}
+                onChange={handleDateChange}
+                dateFormat="dd/MM/yyyy"
+                placeholderText="Select a date"
+                className={Styles.dateInput}
+                required
+              />
+              <button onClick={handleFillDate}>Current</button>
+            </div>
+            <div className={Styles.content}>
+              <p>Shift No. : </p>
+              <select
+                value={shift}
+                onChange={handleShift}
+                className={Styles.input}
+                required
+              >
+                <option value="">Select an option</option>
+                <option value="option1">1</option>
+                <option value="option2">2</option>
+                <option value="option3">3</option>
+              </select>
+            </div>
+            <div className={Styles.content}>
+              <p>Vehicle : </p>
+              <select
+                value={vehicle}
+                onChange={handleVehicle}
+                className={Styles.input}
+                style={{ width: "7rem" }}
+              >
+                <option value="">Excavator</option>
+                <option value="Ve-3">Ve-3</option>
+                <option value="Ve-4">Ve-4</option>
+                <option value="Ve-5">Ve-5</option>
+                <option value="Ve-6">Ve-6</option>
+                <option value="Ve-7">Ve-7</option>
+                <option value="KVE-9">KVE-9</option>
+                <option value="KVE-10">KVE-10</option>
+                <option value="KVE-11">KVE-11</option>
+                <option value="KVE-12">KVE-12</option>
+                <option value="KVE-13">KVE-13</option>
+                <option value="KVE-14">KVE-14</option>
+              </select>
 
-            <button type="button" onClick={handleSubmit}>
-              Submit
-            </button>
-          </div>
-        </div>
-        <div className={Styles.section}>
-          <div className={Styles.content}>
-            <div>
-              <p>Starting time : </p>
+              <button type="submit">
+                Submit
+              </button>
             </div>
-            <div className={Styles.time}>
-              <input
-                type="text"
-                value={startTime}
-                onChange={(e) => setStartTime(e.target.value)}
+          </div>
+          <div className={Styles.section}>
+            <div className={Styles.content}>
+              <div>
+                <p>Starting time : </p>
+              </div>
+              <div className={Styles.time}>
+                <input
+                  type="text"
+                  value={startTime}
+                  onChange={(e) => setStartTime(e.target.value)}
+                  className={Styles.input}
+                  required
+                />
+                <button onClick={handleFillStartTime}>Current</button>
+              </div>
+            </div>
+            <div className={Styles.content}>
+              <div>
+                <p>Ending time : </p>
+              </div>
+              <div className={Styles.time}>
+                <input
+                  type="text"
+                  value={endTime}
+                  onChange={(e) => setEndTime(e.target.value)}
+                  className={Styles.input}
+                  required
+                />
+                <button onClick={handleFillEndTime}>Current</button>
+              </div>
+            </div>
+          </div>
+          <div className={Styles.section}>
+            <div className={Styles.content}>
+              <p>Idle Hours : </p>
+              <select
+                value={idleHours}
+                onChange={handleIdleHours}
                 className={Styles.input}
-              />
-              <button onClick={handleFillStartTime}>Current</button>
+              >
+                <option value="">Select an option</option>
+                <option value="NRT pick up/drop">NRT pick up/drop</option>
+                <option value="Relieving">Relieving</option>
+                <option value="Bad weather/Heavy Rainfall">
+                  Bad weather/Heavy Rainfall
+                </option>
+                <option value="Safety Related Pause">
+                  Safety Related Pause
+                </option>
+                <option value="No Internal Transport">
+                  No Internal Transport
+                </option>
+                <option value="Late Duty Bus">Late Duty Bus</option>
+                <option value="Public/Local Authority Problem">
+                  Public/Local Authority Problem
+                </option>
+                <option value="Blasting">Blasting</option>
+                <option value="Union Related Issues">
+                  Union Related Issues
+                </option>
+                <option value="Canteen Issue">Canteen Issue</option>
+              </select>
             </div>
           </div>
-          <div className={Styles.content}>
-            <div>
-              <p>Ending time : </p>
-            </div>
-            <div className={Styles.time}>
-              <input
-                type="text"
-                value={endTime}
-                onChange={(e) => setEndTime(e.target.value)}
-                className={Styles.input}
-              />
-              <button onClick={handleFillEndTime}>Current</button>
-            </div>
-          </div>
-        </div>
-        <div className={Styles.section}>
-          <div className={Styles.content}>
-            <p>Idle Hours : </p>
-            <select
-              value={idleHours}
-              onChange={handleIdleHours}
-              className={Styles.input}
-            >
-              <option value="">Select an option</option>
-              <option value="NRT pick up/drop">NRT pick up/drop</option>
-              <option value="Relieving">Relieving</option>
-              <option value="Bad weather/Heavy Rainfall">
-                Bad weather/Heavy Rainfall
-              </option>
-              <option value="Safety Related Pause">Safety Related Pause</option>
-              <option value="No Internal Transport">
-                No Internal Transport
-              </option>
-              <option value="Late Duty Bus">Late Duty Bus</option>
-              <option value="Public/Local Authority Problem">
-                Public/Local Authority Problem
-              </option>
-              <option value="Blasting">Blasting</option>
-              <option value="Union Related Issues">Union Related Issues</option>
-              <option value="Canteen Issue">Canteen Issue</option>
-            </select>
-          </div>
-        </div>
+        </form>
       </div>
     </>
   );
